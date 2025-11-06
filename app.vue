d0d40ecf3aef57bb32624f4b434b40ad42b98354
jaksatomovic <jaksa.tomovic@gmail.com>
2025-11-06 18:28:58 +0100

    Add Umami script via app.vue useHead - exact format

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+// Add Umami analytics script exactly as provided
+useHead({
+  script: [
+    {
+      src: 'https://cloud.umami.is/script.js',
+      defer: true,
+      'data-website-id': '7fa05be7-2d37-4c40-ae0b-8ca27bfc156e'
+    }
+  ]
+})
+</script>
+
